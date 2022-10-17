a1eeb3268752865161055e39f999527b9f5637df
Medardo Antonio Rodriguez <med.merchise@gmail.com>
2022-10-17 15:25:03 -0400

    Improve 'mappings' module

--- xit/mappings.py
+++ xit/mappings.py
@@ -1,6 +1,7 @@
 """General mappings tools."""
 
 from collections.abc import Mapping
+from typing import Callable
 
 
 def flatten_dict(source: Mapping, sep: str = '.', prefix: str = '') -> dict:
@@ -15,36 +16,70 @@ def flatten_dict(source: Mapping, sep: str = '.', prefix: str = '') -> dict:
     return res
 
 
-def pop_items(source: dict, *keys) -> dict:
-    """Pop a collection of keys from a source dictionary."""
-    return {key: source.pop(key, None) for key in keys if key in source}
+def unflatten_dict(source: Mapping, sep: str = '.') -> dict:
+    """Get a unflatten dictionary."""
+    res = {}
+    for key, value in source.items():
+        if sep in key:
+            pivot = res
+            *parts, tail = key.split(sep)
+            for part in parts:
+                pivot = pivot.setdefault(part, {})
+            pivot[tail] = value
+        else:
+            res[key] = value
+    return res
 
 
-def asdict(obj: object) -> dict[str, object]:
-    """Return a new dictionary mapping from a given object.
+def pop_items(source: dict, *keys, **defaults) -> dict:
+    """Get a dictionary by popping from 'keys' and 'default' arguments."""
+    res = {key: source.pop(key) for key in keys if key in source}
+    for key, value in defaults.items():
+        res.setdefault(key, source.pop(key, value))
+    return res
 
-    The function applies recursively to field values that are
-    dataclass instances.
+
+def asdict(
+    obj: object, filter_key: str | Callable[[str], bool] | None = '_'
+) -> dict[str, object]:
+    """Return a mapping from a given object.
+
+    Parameter `filter_key` could be: a string (keys starting with that prefix
+    won't be included), a callable to check which keys are ok to be included,
+    or None in which case a key is checked to be a string.  A common filter
+    could be `str.isupper`.
 
     """
-    from collections.abc import Mapping
+    # TODO: support `attrs` library?
     import dataclasses as dc
 
+    match filter_key:
+        case str(prefix):
+            ok = lambda key: not key.startswith(prefix)  # noqa: E731
+        case callable(check):
+            ok = check
+        case None:
+            ok = lambda key: isinstance(key, str)  # noqa: E731
+        case _:
+            raise TypeError(filter_key)
     if not isinstance(obj, type):
-        if callable(getattr(obj, 'asdict', None)):
-            return obj.asdict()
-        elif dc.is_dataclass(obj):
-            return dc.asdict(obj)
-        elif callable(getattr(obj, 'dict', None)):  # pydantic
-            return obj.dict()
-        elif isinstance(obj, Mapping):
-            return dict(obj)
-        else:
-            tname = type(obj).__name__
-            raise TypeError(
-                f"asdict() should not be called on '{tname}' instances."
-            )
+        try:
+            if callable(getattr(obj, 'asdict', None)):
+                res = obj.asdict()
+            elif dc.is_dataclass(obj):
+                res = dc.asdict(obj)
+            elif callable(getattr(obj, 'dict', None)):  # pydantic
+                res = obj.dict()
+            else:
+                res = dict(obj)
+            res = res.items()
+        except Exception:
+            res = None
     else:
-        raise TypeError(
-            f"asdict() should not be called on '{obj.__name__}' type."
-        )
+        res = None
+    if res is None:
+        try:
+            res = obj.__dict__.items()
+        except AttributeError:
+            res = ((key, getattr(obj, key)) for key in dir(obj))
+    return {key: value for key, value in res if ok(key)}
